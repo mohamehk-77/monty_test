e0bddf8948e523ee589739791081eb401de1f171
mohamed kamal <kmido9729@gmail.com>
2023-11-16 22:52:41 +0200

    fix 0 monttyy

--- bytecodes/01.m
+++ bytecodes/01.m
@@ -3,13 +3,13 @@ push 1 Push 1 onto the stack
 
 push 2
   push 3
-                   pall
+                   pall    
 
 
-
+                           
 push 4
 
-    push 5
-      push    6
+    push 5    
+      push    6        
 
 pall This is the end of our program. Monty is awesome!
